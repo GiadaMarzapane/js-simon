9c137f941ee2ffb7557ec263ccbbe484f5dd2ef0
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-13 16:59:55 +0100

    Add STEP 4

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 OK// 1- creare funzione per generare numeri random da 1 a 50
 OK// 2- creare ciclo for per stampare in pagina 5 numeri generati random
 (pusharli in un array?)
-3- creare un timer di 30 secondi, dopo il quale i numeri inseriti scompaiono
-4- creare 5 prompt() in cui all'utente si deve chiedere di inserire i numeri appena scomparsi in ordine (?)
+OK// 3- creare un timer di 30 secondi, dopo il quale i numeri inseriti scompaiono
+OK// 4- creare 5 prompt() in cui all'utente si deve chiedere di inserire i numeri appena scomparsi in ordine (?)
 5- stampare quanti e quali dei numeri da indovinare sono stati indovinati.
 <!-------------------FINE STEP---------------------------------->
--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ console.log(numberToGuess);
 /* -------------------------------------------------- */ 
 
 setTimeout(hidden, mySeconds(5));
-
+setTimeout(createPrompt, mySeconds(7));
 
 /*------------------ FUNCTIONS ------------------*/
 function generateRandomNumber (min, max) {
@@ -40,6 +40,29 @@ function mySeconds(seconds) {
 function hidden () {
     alert('I numeri sono scomparsi')
     const myContainer = document.querySelector('div.container.flex');
+    const divMyNumbers = document.querySelector('div.container.hidden');
     myContainer.classList.remove('flex');
     myContainer.classList.add('hidden');
+    divMyNumbers.classList.remove('hidden');
+    divMyNumbers.classList.add('flex');
+}
+
+function createPrompt () {
+    for (let index = 1; index <= 5; index++) {
+
+        let guessNumbers = parseInt(prompt('Inerisci il ' + index + ' ° numero della sequenza'));
+
+        while (isNaN(guessNumbers) == true){
+            guessNumbers = parseInt(prompt('VALORE NON VALIDO, Inerisci il ' + index + ' ° numero della sequenza'));
+        }
+        const spanMyNumbers = document.querySelector('#numeri-inseriti');
+        const mySpan = document.createElement('span');
+        mySpan.innerHTML = guessNumbers;
+        mySpan.classList.add('number');
+        spanMyNumbers.append(mySpan); 
+
+        // if(isNaN(guessNumbers) == false){
+
+        // }
+    }
 }
