3f451ee1da2c168edc09af3e28a4db9cdc4b999b
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-13 15:38:19 +0100

    Update STEP2 + add CSS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const numberToGuess = [];
 
 for (let index = 1; index <= 5; index++) {
-
+    
     let myRandomNumbers = generateRandomNumber(1, 50);
 
     while (numberToGuess.includes(myRandomNumbers)) {
         myRandomNumbers = generateRandomNumber(1, 50)
     }
     numberToGuess.push(myRandomNumbers);
+
+    const addNumbers = createMySpan(myRandomNumbers);
+    
 }
 
 console.log(numberToGuess);
@@ -18,4 +21,12 @@ function generateRandomNumber (min, max) {
     
         return Math.ceil(Math.random() * (max - min)) + min;
         
+}
+
+function createMySpan (element){
+    const myContainer = document.querySelector('div.container');
+    const mySpan = document.createElement('span');
+    mySpan.classList.add('number');
+    mySpan.innerHTML = element;
+    myContainer.append(mySpan);
 }
